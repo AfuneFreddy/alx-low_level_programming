51b170454a34b4233598b14eb294ce0cc7406ed9
AfuneFreddy <freddyotieno093@gmail.com>
2022-12-09 14:00:03 -0800

    commit task 09

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,10 +9,10 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i < 58; i++)
+	for (i = 0; i < 100; i++)
 	{
-		putchar(i);
-		if (i != 57)
+		putchar(i + '0');
+		if (i < 99)
 		{
 			putchar(',');
 			putchar(' ');
