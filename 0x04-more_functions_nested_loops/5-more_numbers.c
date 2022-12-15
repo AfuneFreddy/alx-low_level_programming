7ae429d5f4e901c960b90ff00336762ad71f0852
AfuneFreddy <freddyotieno093@gmail.com>
2022-12-15 12:38:00 -0800

    commiting task 05

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers between 0 to 14
+ * 10 times.
+ * Return: no return.
+ */
+void more_numbers(void)
+{
+	int i, ro;
+
+	for (ro = 0; ro <= 9; ro++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
+
+
