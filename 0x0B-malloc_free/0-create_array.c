dc8a258b3593ee66d18df446da917432e8fc493b
AfuneFreddy <freddyotieno093@gmail.com>
2023-01-12 10:46:47 -0800

    task 00 of malloc assigment

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * create_array - creates an array of chars.
+ * @size: size of the array.
+ * @c: storaged char
+ *
+ * Return: pointer of an array of chars
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *buffer;
+	unsigned int position;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	buffer = (char *) malloc(size * sizeof(c));
+	if (buffer == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		position = 0;
+		while (position < size)
+		{
+			*(buffer + position) = c;
+			position++;
+		}
+		return (buffer);
+	}
+}
