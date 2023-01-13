9756218545003e6778541afe3c6450fe1ea1ede9
AfuneFreddy <freddyotieno093@gmail.com>
2023-01-13 06:02:46 -0800

    updating task 04 of more malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,31 +12,16 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *clone;
-	char *relloc;
-	unsigned int i;
-
-	if (ptr != NULL)
-		clone = ptr;
-	else
-	{
-		return (maloc(new_size));
-	}
-	if (new_size == old_size)
-		return (ptr);
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
-		return (0);
+		return (NULL);
 	}
-	relloc = malloc(new_size);
-	if (relloc == NULL)
-		return (0);
-	for (i = 0; i < (old_size || i < new_size); i++)
-	{
-		*(relloc + i) = clone[i];
-	}
+	if (ptr == NULL)
+		ptr = malloc(new_size);
+	if (new_size == old_size)
+		return (ptr);
 	free(ptr);
-	return (relloc);
+	ptr = malloc(new_size);
+	return (ptr);
 }
-
